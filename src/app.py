a5ad66c7bec798fa4e34ea32ff2ae38f8c56b99c
Ezequiel Merino <emerino@aistechnology.es>
2023-05-10 12:48:03 +0200

    enable load tokens changing to json

--- src/app.py
+++ src/app.py
@@ -1,13 +1,17 @@
 import asyncio
 import telegram
-import os
+import json
 
+def loadconfig():
+    f = open("env.json", "r")
+    string_double_quotes = f.read()
+    return(json.loads(string_double_quotes))
 
-async def main():
-    bot = telegram.Bot(os.environ.get('BOT_TOKEN'))
+async def main(conf):
+    bot = telegram.Bot(str(conf['BOT_TOKEN']))
     async with bot:
         print(await bot.get_me())
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    asyncio.run(main(loadconfig()))
